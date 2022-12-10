573650b17e810fff590cff2a620dad71eb8a7b3e
kelvin porbeni <kelvinporbeni456@gmail.com>
2022-12-09 18:13:13 -0800

    update

---
+++ 0x00-hello_world/0-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point for all C program
+ * @void: Doesn't take any parameters
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x = 1;
+printf("%d\n", x);
+return (0);
+}
