3d45e93754d2212568d28962aea24b2310c4c7be
kelvin porbeni <kelvinporbeni456@gmail.com>
2022-12-12 16:00:41 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,14 @@
  * followed by a new line
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	char ch = 'a';
 
 	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		putchar(ch);
-		putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 }
