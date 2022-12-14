29e6808cd0744563eaee011a8307c5ee50470121
kelvin porbeni <kelvinporbeni456@gmail.com>
2022-12-14 05:00:47 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the character _putchar
+ * followed by a new line
+ * Return : Always 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	printf("\n");
+
+	return (0);
+}
